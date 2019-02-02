cfbaa3107e4d210673f65526f7e30678a6fa12ee
Flavio Andrade Machado <flaviochess@gmail.com>
2019-02-02 12:34:29 -0200

    initial config

---
+++ TwitterReportAPI/src/main/java/br/com/evaluation/twitterreportapi/sample/SampleController.java
@@ -0,0 +1,16 @@
+package br.com.evaluation.twitterreportapi.sample;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sample")
+public class SampleController {
+
+    @GetMapping
+    public String sample() {
+
+        return "ok";
+    }
+}
